71611f4bfc72cd9140b2694075282ae6d5183bd3
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-02-25 16:21:12 +0200

    react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todo from './components/todo';
+
+class App extends Component {
+  render() {
+    return (
+      <Todo />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Todo extends React.Component {
+    render() {
+        return(
+            <div>todo</div>
+        );
+    }
+}
+export default Todo;
