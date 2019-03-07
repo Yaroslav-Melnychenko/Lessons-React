cd756ecbdc72a99b423a0c0a0074783936b74fd2
Yaroslav Melmychenko <yaroslav.melnychenko@chisw.com>
2019-03-07 12:16:47 +0200

    init new files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
-import Todo from './components/todo';
+import Navbar from './components/Navbar'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
   render() {
     return (
-      <Todo />
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
